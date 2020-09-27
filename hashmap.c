699550d3baeb8882befeb5be63ddb6631b68c5d4
repl.it user <replituser@example.com>
2020-09-27 16:33:28 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -47,7 +47,7 @@ int is_equal(void* key1, void* key2){
 void insertMap(HashMap * map, char * key, void * value) { //insertar datos
   long posicion = hash(key, map->capacity);
   while(map->buckets[posicion] != NULL){
-    posicion *= 1;
+    posicion += 1;
   }
   map->buckets[posicion]=createPair(key,value);
   map->size = posicion;
