b36118f40873a2ee63c484d4fb09a9927af119f2
repl.it user <replituser@example.com>
2020-09-27 16:55:24 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -50,7 +50,7 @@ void insertMap(HashMap * map, char * key, void * value) { //insertar datos
     posicion *= 1;
   }
   map->buckets[posicion]=createPair(key,value);
-  map->size = posicion;
+  map->current = posicion;
 }
 
 void enlarge(HashMap * map) {
