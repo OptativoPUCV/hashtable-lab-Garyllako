dacd58efef9f40979b2efe120062281f8bb16049
repl.it user <replituser@example.com>
2020-09-27 03:18:31 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -62,7 +62,7 @@ void enlarge(HashMap * map) {
 HashMap * createMap(long capacity) {
     HashMap * map = (HashMap *) malloc(sizeof(long));
     map->buckets = NULL;
-    map->size =(long) malloc(sizeof(long));
+    map->size = (long) malloc(sizeof(Pair));
     map->capacity = capacity;
     map->current = -1;
     return map;
