9d05fc820758da0f154ad890b0ebc93e0b23b4fd
repl.it user <replituser@example.com>
2020-09-28 02:00:48 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -112,7 +112,7 @@ void * firstMap(HashMap * map) {
 void * nextMap(HashMap * map) {
     long posicion;
     for(posicion = (map->current + 1); posicion < map->size ;posicion++){
-      if(map->buckets[posicion]->value != NULL){
+      if((map->buckets[posicion] != NULL)){
         printf("%ld",posicion);
         map->current = posicion;
         return map->buckets[posicion]->value;
