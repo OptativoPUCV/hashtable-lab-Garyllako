44bcd2777d26ec9daa7df2bf686a4df9f9351dd1
repl.it user <replituser@example.com>
2020-09-28 01:15:53 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -74,14 +74,12 @@ HashMap * createMap(long capacity) {
 
 void eraseMap(HashMap * map,  char * key) {    
     long posicion = hash(key, map->capacity);
-    //while(is_equal(map->buckets[posicion]->key,key) != 1){
-    //  posicion += 1;
-    //  if(posicion >= map->capacity){
-    //    posicion = posicion - map->capacity;
-    //  }
-    //}
-    map->buckets[posicion]->key = NULL;
-    map->size -= 1;
+    for(posicion = 0; posicion < map->size ;posicion++){
+      if(is_equal(map->buckets[posicion]->key,key) != 1){
+        map->buckets[posicion]->key = NULL;
+        map->size -= 1;
+      }
+    }
 }
 
 void * searchMap(HashMap * map,  char * key) {   
